30b478c2f4707e78c3d1753a8a5b3d49c795b506
bguemos <bri-lynn@hotmail.com>
2024-02-15 23:22:29 -0800

    why rescue page

--- src/pages/whyAdopt.tsx
+++ src/pages/whyAdopt.tsx
@@ -1,11 +1,66 @@
 import Image from 'next/image'
 import Navbar from '@/components/Navbar'
 import LargeButton from '@/components/LargeButton'
+import styles from '../styles/why.module.css'
+import BarChart from '@/components/BarChart'
+
+
 
 export default function WhyAdopt() {
     return (
-        <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-            <Navbar />
+        <>
+  
+        <main className={`flex min-h-screen w-430 flex flex-col `}>
+        <Navbar />
+            <div className={`flex flex-col p-10 items-start gap-4`}>
+            <h1 className={`text-3xl font-extrabold`}>Why Rescue</h1>
+            <div style={{ width: '314px' }}>
+            <p>Rescuing dogs is a compassionate and socially responsible choice that positively impacts both the individual and the broader community. </p>
+            <p style={{marginTop:'20px'}}>By adopting a dog from a shelter or rescue organization, you provide a loving home to an animal in need, offering them a second chance at a fulfilling life</p>
+            </div>
+
+            <h3 className={`text-2xl font-semibold`}>Reasons to Rescue</h3>
+            <div className={`flex flex-row p-10 items-center gap-4`}>
+            <div className={`flex flex-col items-center pt-1`}>
+            
+                <Image className={styles.dogimage}
+                    src='/whyimgs/smiledog.jpg'
+                    height={60}
+                    width={100}
+                    alt='dog'
+                />
+                <p>Find Happiness</p>
+                
+            </div>
+            <div className={`flex flex-col items-center pt-1`}>
+            
+            <Image className={styles.dogimage}
+                src='/whyimgs/labs.jpg'
+                height={60}
+                width={100}
+                alt='dog'
+            />
+            <p>Make a Difference</p>
+            
+        </div>
+        <div className={`flex flex-col items-center pt-1`}>
+            
+            <Image className={styles.dogimage}
+                src='/whyimgs/golden.jpg'
+                height={60}
+                width={100}
+                alt='dog'
+            />
+            <p>Feel Unconditional love</p>
+            
+        </div>
+
+            </div>
+            <h3 className={`text-2xl font-semibold`}>Backed By Research</h3>
+       
+            <BarChart/>
+            </div>
         </main>
+        </>
     )
 }
