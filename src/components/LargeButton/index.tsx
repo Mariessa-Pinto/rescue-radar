c264e598f95d2b94f930e34e1f4b15b7f69293e6
Mariessa-Pinto <mpinto13@my.bcit.ca>
2024-02-17 12:29:32 -0800

    home made responsive

--- src/components/LargeButton/index.tsx
+++ src/components/LargeButton/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const LargeButton = ({ text, link }: ILargeButton) => {
     return (
         <Link href={link}>
-            <button className={`rounded-full border-2 w-60 h-14 border-blue font-outfit text-xl font-medium bg-white shadow-lg`}>
+            <button className={`rounded-full border-2 w-60 h-14 border-blue font-outfit text-h4 font-medium bg-white shadow-lg lg:w-96 lg:h-16 lg:text-wh2`}>
                 {text}
             </button>
         </Link>
