197428e9e905a90fad71f1b3545bfda3a392e5ca
Mariessa-Pinto <mpinto13@my.bcit.ca>
2024-02-20 10:19:23 -0800

    updated config

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
   images: {
     domains: ['dl5zpyw5k3jeb.cloudfront.net'],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/adoptapet.me/sb',
-  //       destination: 'https://api.adoptapet.me/sb', // Replace with actual API URL
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = nextConfig;
