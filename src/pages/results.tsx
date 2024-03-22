84216566253fb0e3acc1f6199e9360b0c722c67a
anikagrewal <a.grewal.work@gmail.com>
2024-03-22 01:11:57 -0700

    loading and layout

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -5,12 +5,12 @@ import LargeButton from '@/components/LargeButton'
 
 export default function Results() {
     return (
-        <main className={`flex min-h-screen flex-col items-center justify-between p-5`}>
+        <main className={`flex min-h-screen flex-col items-center justify-between p-5 lg:bg-gradient-to-b from-blue to-red`}>
         
             <Navbar />
            
             <AdoptAPet/>
-            <div className={` flex bg-red rounded-t-xl h-full flex justify-center w-400`}>
+            <div className={` flex bg-red rounded-t-xl h-full flex justify-center w-400 lg:hidden`}>
                     <div className={`h-full w-12 bg-red rounded-full rounded-lg`}> 
                     <div className={`flex justify-center`}>
                     <LargeButton
